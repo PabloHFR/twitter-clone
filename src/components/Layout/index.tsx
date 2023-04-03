9cdf249c5fb160ab4622d825ab28fd5c1b22d52b
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-04-03 19:51:24 -0300

    Added basic layout structure

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
