fcd612a18482ea708d09fe991688f2738ac0a4e2
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-04-07 08:15:59 -0300

    Added Login Modal component with Zustand for state management

--- src/components/Modals/index.tsx
+++ src/components/Modals/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/Button";
+import { Button } from "@/components/Button";
 import { useCallback } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { Layout } from "@/components/Layout";
-import { Modal } from "@/Modal";
+import { LoginModal } from "@/components/Modals/LoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal actionLabel="Submit" isOpen title="Test modal" />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
