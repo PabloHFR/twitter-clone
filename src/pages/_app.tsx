40d21ce7f6aaeef2fbd99286c31042f43f20bf73
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-04-07 19:06:40 -0300

    Added Register Modal component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { Layout } from "@/components/Layout";
 import { LoginModal } from "@/components/Modals/LoginModal";
+import { RegisterModal } from "@/components/Modals/RegisterModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <RegisterModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
