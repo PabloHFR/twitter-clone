9b63004c9c3de645c41ded563c9ed3157a25df11
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-04-25 09:09:50 -0300

    Added buttons to Post Item

--- src/components/PostFeed/PostItem.tsx
+++ src/components/PostFeed/PostItem.tsx
@@ -4,7 +4,7 @@ import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/router";
 import { useCallback, useMemo } from "react";
 import { Avatar } from "../Avatar";
-import { AiOutlineMessage } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -73,7 +73,19 @@ export function PostItem({ data, userId }: PostItemProps) {
             <span className="text-neutral-500 text-sm">{createdAt}</span>
           </div>
           <div className="text-white mt-1">{data.body}</div>
-          <div className="flex flex-row items-center mt-3 gap-10"></div>
+          <div className="flex flex-row items-center mt-3 gap-10">
+            <div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-sky-500">
+              <AiOutlineMessage size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+            <div
+              onClick={onLike}
+              className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+            >
+              <AiOutlineHeart size={20} />
+              <p>{data.comments?.length || 0}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
